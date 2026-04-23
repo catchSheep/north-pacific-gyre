28ebf51ecb0eb340475dacddacbff90d14067956
catchSheep <81134674+catchSheep@users.noreply.github.com>
2026-04-23 14:18:09 +0800

    added max

--- goostats.sh
+++ goostats.sh
@@ -4,4 +4,4 @@ echo "working with file: ${fname}"
 echo "Welcome to Nelle's stats script"
 # Compute the min/max/range of values in a file
 min=$( cat ${fname} | sort | head -1)
-
+max=$( cat ${fname} | sort | tail -1)
